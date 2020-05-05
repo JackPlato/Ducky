c9889b2c3028d91346a930d346cd535cc82ce0b1
JackPlato <58954719+JackPlato@users.noreply.github.com>
2020-05-05 15:52:32 -0700

    Big Modular Prefab Push / GameOverScript add / more imports

---
+++ Assets/SCRIPTS/GameOverScreenScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreenScript : MonoBehaviour
+{
+    public Text gameOverText, scoreText, continueText;
+
+    void Start()
+    {
+        gameOverText.text = "You Died!";
+        //scoreText.text = "But hey, you nabbed" + FINALSCORE + "along the way";
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
