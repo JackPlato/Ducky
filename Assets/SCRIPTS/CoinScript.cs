2aaeedf46da5f8c71e819782bb8221791d71503a
JackPlato <58954719+JackPlato@users.noreply.github.com>
2020-05-05 12:07:43 -0700

    Big organization changes, coin prefabs made

---
+++ Assets/SCRIPTS/CoinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public int value;
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 0.3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //add value to player score
+            Destroy(gameObject);
+        }
+    }
+}
