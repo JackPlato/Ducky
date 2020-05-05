4ed10d866faa7549a186c486269abb0111cca07d
JackPlato <58954719+JackPlato@users.noreply.github.com>
2020-05-05 12:42:48 -0700

    Alarm light prefab updated
    Alarm light now spins; small imports made as well

---
+++ Assets/SCRIPTS/AlarmLightScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmLightScript : MonoBehaviour
+{
+    public float spin = 230.0f;
+    public GameObject lightOne, lightTwo;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        lightOne.transform.RotateAround(transform.position, Vector3.up, spin * Time.deltaTime);
+        lightTwo.transform.RotateAround(transform.position, Vector3.up, spin * Time.deltaTime);
+    }
+}
--- Assets/SCRIPTS/CoinScript.cs
+++ Assets/SCRIPTS/CoinScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CoinScript : MonoBehaviour
 {
     public int value;
+    float spin = 0.5f;
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.3f);
+        transform.Rotate(0, 0, spin);
     }
 
     private void OnTriggerEnter(Collider other)
