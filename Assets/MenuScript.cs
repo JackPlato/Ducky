84b469da0b1efda6a92690c2e9dba0ad4658bcd5
JackPlato <58954719+JackPlato@users.noreply.github.com>
2020-05-05 10:37:42 -0700

    created menu script

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button startButton, creditsButton, quitButton;
+    public GameObject titleScreen, creditsScreen, gameOverScreen;
+    private bool credits = false;
+
+    void Start()
+    {
+        titleScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+        gameOverScreen.SetActive(false);
+
+        startButton.onClick.AddListener(StartGame);
+        creditsButton.onClick.AddListener(Credits);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    void Update()
+    {
+        if (credits && (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Cancel")))
+        {
+            Credits();
+        }
+    }
+
+    void StartGame()
+    {
+
+    }
+    
+    void Credits()
+    {
+        credits = !credits;
+        if (credits)
+        {
+            titleScreen.SetActive(false);
+            creditsScreen.SetActive(true);
+        }
+        if (!credits)
+        {
+            titleScreen.SetActive(true);
+            creditsScreen.SetActive(false);
+        }
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
