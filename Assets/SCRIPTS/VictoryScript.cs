8cc856e4465f47d31890b88741ff4893bf4cb673
JackPlato <58954719+JackPlato@users.noreply.github.com>
2020-05-06 10:04:23 -0700

    small push

---
+++ Assets/SCRIPTS/VictoryScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VictoryScript : MonoBehaviour
+{
+    public Text scoreText;
+    public GameObject victoryScreen;
+    void OnTriggerEnter(Collider other)
+    {
+        victoryScreen.SetActive(true);
+        //scoreText.text = "And you got $" + FINALSCORE + " too!";
+    }
+
+    private void Update()
+    {
+        if(victoryScreen && Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("GameScene_Bel", LoadSceneMode.Single);
+        }
+    }
+}
