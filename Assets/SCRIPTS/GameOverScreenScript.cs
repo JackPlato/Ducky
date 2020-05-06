e620aade18d695c37f4fc79163926cdc1277e25f
JackPlato <58954719+JackPlato@users.noreply.github.com>
2020-05-05 17:21:40 -0700

    Big asset / script push

--- Assets/SCRIPTS/GameOverScreenScript.cs
+++ Assets/SCRIPTS/GameOverScreenScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreenScript : MonoBehaviour
 {
@@ -10,12 +11,27 @@ public class GameOverScreenScript : MonoBehaviour
     void Start()
     {
         gameOverText.text = "You Died!";
-        //scoreText.text = "But hey, you nabbed" + FINALSCORE + "along the way";
+        //scoreText.text = "But hey, you nabbed" + FINALSCORE + "\nalong the way";
+        scoreText.color = new Color(scoreText.color.r, scoreText.color.g, scoreText.color.b, 0);
+        continueText.color = new Color(continueText.color.r, continueText.color.g, continueText.color.b, 0);
+        StartCoroutine(TextCoroutine());
     }
 
 
     void Update()
     {
-        
+        if (continueText.color.a > 0 && Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+        }
+    }
+
+    IEnumerator TextCoroutine()
+    {
+        yield return new WaitForSeconds(1);
+        scoreText.color = new Color(scoreText.color.r, scoreText.color.g, scoreText.color.b, 255);
+        yield return new WaitForSeconds(1);
+        continueText.color = new Color(continueText.color.r, continueText.color.g, continueText.color.b, 255);
+
     }
 }
---
+++ Assets/SCRIPTS/PitfallScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitfallScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //player dies, game over screen activates
+        }
+    }
+}
